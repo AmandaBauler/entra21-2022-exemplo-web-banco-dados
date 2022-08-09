8f6ad4289e17e50d6470fae9eaf814dfd0955555
amanda.bauler <amanda.bauler@hotmail.com>
2022-08-08 22:14:07 -0300

    Continuação do exemplo Clinica Veterinária

--- Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entra21.CSharp.ClinicaVeterinaria.Repositorio.BancoDados;
+using Entra21.CSharp.ClinicaVeterinaria.Servico;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
 {
     //Dois pontos herança(mais pra frente)
     public class RacaController : Controller
-    {   /// <summary>
+    {
+        private RacaServico racaServico;
+
+        // Construtor: Objetivo construir o objeto RacaController,
+        //com o minimo necessário para o funcionamento correto
+        public RacaController(ClinicaVeterinariaContexto contexto)
+        {
+            racaServico = new RacaServico(contexto);
+        }
+        /// <summary>
         /// Endpoint que permite listar todas as raças
         /// </summary>
         /// <returns>Retorna a página html com as raças</returns>
@@ -28,6 +39,8 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
             [FromQuery] string nome,
             [FromQuery] string especie)
         {
+            racaServico.Cadastrar(nome, especie);
+
             return RedirectToAction("Index");
         }
 
---
+++ Entra21.CSharp.ClinicaVeterinaria/Repositorio/RacaRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using Entra21.CSharp.ClinicaVeterinaria.Repositorio.BancoDados;
+using Entra21.CSharp.ClinicaVeterinaria.Repositorio.Entidades;
+
+namespace Entra21.CSharp.ClinicaVeterinaria.Repositorio
+{
+    public class RacaRepositorio : IRacaRepositorio
+    {
+        private readonly ClinicaVeterinariaContexto _contexto;
+        public RacaRepositorio(ClinicaVeterinariaContexto contexto)
+        {
+            _contexto = contexto;
+        }
+        public void Cadastrar(Raca raca)
+        {
+            //nova maneira de fazer um INSERT na tabela
+            _contexto.Racas.Add(raca);
+            _contexto.SaveChanges();
+        }
+    }
+}
