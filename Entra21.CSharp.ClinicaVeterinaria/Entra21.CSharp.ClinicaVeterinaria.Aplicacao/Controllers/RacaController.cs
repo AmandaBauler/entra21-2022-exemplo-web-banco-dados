4b8aea1552ce5f28b5484d2894d9a7f40fc33677
amanda.bauler <amanda.bauler@hotmail.com>
2022-08-17 20:41:02 -0300

    Refatoração no código.

--- Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
@@ -43,9 +43,9 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
             return View();
         }
 
-        [Route("/raca/registrar")]
+        [Route("/raca/cadastrar")]
         [HttpPost]
-        public IActionResult Registrar(
+        public IActionResult Cadastrar(
             [FromForm] string nome,
             [FromForm] string especie)
         {
@@ -79,13 +79,13 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
         }
 
         [HttpPost]
-        [Route("/raca/alterar")]
-        public IActionResult Alterar(
+        [Route("/raca/editar")]
+        public IActionResult Editar(
             [FromForm] int id,
             [FromForm] string nome,
             [FromForm] string especie)
         {
-            _racaServico.Alterar(id, nome, especie);
+            _racaServico.Editar(id, nome, especie);
 
             return RedirectToAction("Index");
         }
